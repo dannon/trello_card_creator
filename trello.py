25347694b3f6ee9485db2664cf660d6a01bd8de8
Dannon Baker <dannonbaker@me.com>
2014-04-16 13:27:40 -0400

    Update styling/verbiage.

--- trello.py
+++ trello.py
@@ -61,30 +61,47 @@ $(document).ready(function(){
 id="masthead-inner"> <div class="title"> <a href="http://usegalaxy.org/"><img src="static/galaxyIcon_noText.png"
 style="width: 26px; vertical-align: top;" border="0"> Galaxy</a> </div> </div> </div> <div id="content">
 
-<div><p>
-Galaxy is now using <a href="https://trello.com/board/galaxy-development/506338ce32ae458f6d15e4b3">Trello</a>
-for issue tracking.  Visit the Trello board to see, comment on, and vote for currently open issues.  To create a new
-issue, simply fill out the form below and a card will be created in the "Development Inbox" list of the board.</p></div>
+<div><p class="opening_text">
+<img class="fi_right" src="static/trello.png"/>
+Galaxy uses uses Trello for issue tracking and development planning.  Trello is
+a simple online kanban board that provides a comprehensive view of what's being
+worked on and what the progress is of particular development efforts.<br/> If
+you have an idea for an improvement, or would like to report a bug, please do
+fill out the form below.  Before you do so, however, please check the board to
+ensure that the issue you're reporting doesn't already have an active Trello
+card.  We'd love for everyone to also vote for issues that are important to
+them.  The board is automatically sorted by several heuristics hourly in order
+to make the most important issues (as determined by votes, commentary, etc.)
+bubble to the top of each list.</p>
+
+<p class="opening_text"> <a
+href="https://trello.com/b/75c1kASa/galaxy-development">Visit the Trello
+board</a> to review, comment on, and vote for currently open issues.  To create
+a new issue, simply fill out the form below and a card will be created in the
+"Development Inbox" list of the board.  If you fill out the "Your Trello
+Username" field, you'll be @mentioned on the card in a comment, and should be subscribed to
+future updates.</p></div>
 <div id="interaction_container">
-    <div id="form_container" style="width:50%; float:left">
+    <div id="response_container">
+    </div>
+    <div id="form_container">
         <form method="POST" action="/trello">
-          <label for="id_name">Card Name - A descriptive issue title</label><input id="id_name" name="name" size="80" type="text"/><br/>
-          <label for="id_submitter">Your Trello Username (This will @mention you for notifications)</label><input id="id_submitter" name="submitter" size="80" type="text"/><br/>
-          <label for="id_desc">Verbose Description of the issue/request</label><textarea rows="8" cols="60" id="id_desc" name="desc"
-          type="text"></textarea><br/>
+          <label for="id_name">Card Name</label><input id="id_name" name="name" size="80" type="text" placeholder="Please be descriptive"/><br/>
+          <label for="id_submitter">Your Trello Username</label><input id="id_submitter" name="submitter" size="80" type="text" placeholder="@dannon"/><br/>
+          <label for="id_desc">Description</label><textarea rows="8" cols="60" id="id_desc" name="desc" type="text" placeholder="Detailed description of the issue or feature request.  If it's a bug, please include exact steps to reproduce it."></textarea><br/>
           <input type="submit"/>
         </form>
-    </div>
-    <div id="response_container" style="width:50%; float:right">
     </div>
 </div>
 
 </div> </div></body></html>
 '''
 
+
 @bottle.route('/static/<path:path>')
 def callback(path):
         return bottle.static_file(path, root='static/')
+
 
 @bottle.get('/')
 def create_form():
@@ -116,7 +133,7 @@ def _create_card(name, desc, submitter):
                                         'key': trello_auth_key,
                                         'token': trello_oauth_token})
         comment_req = urllib2.Request(trello_url + "/%s/actions/comments?key=%s&token=%s" % (create_response_dict['id'], trello_auth_key, trello_oauth_token), comment_data)
-        comment_response = json.loads(urllib2.urlopen(comment_req).read())
+        json.loads(urllib2.urlopen(comment_req).read())
     return create_response
 
 if __name__ == "__main__":
